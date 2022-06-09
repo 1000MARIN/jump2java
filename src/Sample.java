26c7599b87b77e73940725fcc9c50628c62b1d1e
1000marin <leexjin@gmail.com>
2022-06-09 15:20:17 +0900

    jump to java

--- src/Sample.java
+++ src/Sample.java
@@ -1,11 +1,12 @@
-import java.util.Locale;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class Sample {
     public static void main(String[] args) {
-        String[] weeks = {"월", "화", "수", "목", "금", "토", "일"};
 
-        for (String week : weeks) {
-            System.out.println(week);
-        }
+        ArrayList<String> pitches = new ArrayList<>(Arrays.asList("123", "153", "139"));
+        pitches.sort(Comparator.naturalOrder());    // 오름차순으로 정렬
+        System.out.println(pitches);                // 123,153,123 출력
     }
 }
