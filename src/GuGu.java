c2c7d57ec3da6c63f15b0c603d2296cef79ab852
1000marin <leexjin@gmail.com>
2022-06-09 13:20:47 +0900

    jump to java

--- src/GuGu.java
+++ src/GuGu.java
@@ -1,22 +1,14 @@
 public class GuGu {
     public void dan(int n) {
-        System.out.println(n * 1);
-        System.out.println(n * 2);
-        System.out.println(n * 3);
-        System.out.println(n * 4);
-        System.out.println(n * 5);
-        System.out.println(n * 6);
-        System.out.println(n * 7);
-        System.out.println(n * 8);
-        System.out.println(n * 9);
+        for (int i = 1; i < 10; i++) {
+            System.out.println(n * i);
+        }
     }
 
     public static void main(String[] args) {
-        int n = 0;
-        for (int i = 1; i < 10; i++) {
-            GuGu guGu = new GuGu();
-            guGu.dan(i);
+        GuGu gugu = new GuGu();
+        for (int i = 2; i < 10; i++) {
+            gugu.dan(i);
         }
-
     }
 }
---
+++ src/Sample.java
@@ -0,0 +1,12 @@
+import java.util.Locale;
+
+public class Sample {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer();
+        sb.append("hello");
+        sb.append(" ");
+        sb.append("jump to java");
+        String result = sb.toString();
+        System.out.println(result);
+    }
+}
