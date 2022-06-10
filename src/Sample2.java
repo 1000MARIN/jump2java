f55d0b3f642867e2f6dfbe1159d14943c234344a
1000marin <leexjin@gmail.com>
2022-06-10 15:26:34 +0900

    jump to java

--- src/Sample2.java
+++ src/Sample2.java
@@ -3,11 +3,9 @@ import java.util.Arrays;
 
 public class Sample2 {
     public static void main(String[] args) {
-        for (int i = 2; i < 10; i++) {
-            for (int j = 1; j < 10; j++) {
-                System.out.println(i * j + " ");
-            }
-            System.out.println("");
+        ArrayList<String> numbers = new ArrayList<>(Arrays.asList("one", "two", "three"));
+        for (String number : numbers) {
+            System.out.println(number);
         }
     }
 }
