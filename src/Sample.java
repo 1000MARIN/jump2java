81f4c41ee65ae59e08149a377f6ac1909136935c
1000marin <leexjin@gmail.com>
2022-06-10 09:39:27 +0900

    jump to java

--- src/Sample.java
+++ src/Sample.java
@@ -1,9 +1,16 @@
 import java.util.*;
 
 public class Sample {
+    enum CoffeeType {
+        AMERICANO,
+        ICE_AMERICANO,
+        CAFE_LATTE
+    };
+
     public static void main(String[] args) {
-        HashSet<String> set = new HashSet<>(Arrays.asList("Jump", "To", "Java"));
-        set.remove("To");
-        System.out.println(set);
+        for (CoffeeType type : CoffeeType.values()) {
+            System.out.println(type);   // 순서대로 출력
+        }
+
     }
 }
