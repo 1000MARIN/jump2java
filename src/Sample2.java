b56011bf47be1c7c5f67babfe0610ad729e234da
1000marin <leexjin@gmail.com>
2022-06-10 15:47:14 +0900

    jump to java

--- src/Sample2.java
+++ src/Sample2.java
@@ -3,10 +3,21 @@ import java.util.Arrays;
 
 public class Sample2 {
     public static void main(String[] args) {
-        ArrayList<String> numbers = new ArrayList<>(Arrays.asList("one", "two", "three"));
-        for (String number : numbers) {
-            System.out.println(number);
+        // 1. 다음 코드의 출력 결과는 무엇일까? (눈으로 해석하고 풀어보자.)
+        String a = "write once, run anywhere";
+        if (a.contains("wife")) {
+            System.out.println("wife");
+        } else if (a.contains("once") && !a.contains("run")) {
+            System.out.println("once");
+        } else if (!a.contains("everywhere")) {
+            System.out.println("everywhere");
+        } else if (a.contains("anywhere")) {
+            System.out.println("anywhere");
+        } else {
+            System.out.println("none");
         }
+
+        // 2. while문을 사용해 1부터 1000까지의 자연수 중 3의 배수의 합을 구해 보자.
     }
 }
 
