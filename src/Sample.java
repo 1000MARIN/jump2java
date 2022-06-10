6dcabdfd0d6172c716f2f17e472f5e37bc3a2805
1000marin <leexjin@gmail.com>
2022-06-10 09:47:55 +0900

    jump to java

--- src/Sample.java
+++ src/Sample.java
@@ -1,16 +1,11 @@
 import java.util.*;
 
 public class Sample {
-    enum CoffeeType {
-        AMERICANO,
-        ICE_AMERICANO,
-        CAFE_LATTE
-    };
 
     public static void main(String[] args) {
-        for (CoffeeType type : CoffeeType.values()) {
-            System.out.println(type);   // 순서대로 출력
-        }
+        String num = "123.456";
+        double d = Double.parseDouble(num);
+        System.out.println(d);
 
     }
 }
