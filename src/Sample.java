044679a6004f1e28d0e54b36abf77c4cddc3a4ed
1000marin <leexjin@gmail.com>
2022-06-10 09:33:20 +0900

    jump to java

--- src/Sample.java
+++ src/Sample.java
@@ -1,15 +1,9 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashMap;
+import java.util.*;
 
 public class Sample {
     public static void main(String[] args) {
-        HashMap<String, String> map = new HashMap<>();
-        map.put("people", "사람");
-        map.put("baseball", "야구");
-        System.out.println(map.keySet());
-
-
+        HashSet<String> set = new HashSet<>(Arrays.asList("Jump", "To", "Java"));
+        set.remove("To");
+        System.out.println(set);
     }
 }
