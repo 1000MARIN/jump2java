b2c63748b0c7f90558a96eeb04b561c1e6c338e8
1000marin <leexjin@gmail.com>
2022-06-20 17:59:54 +0900

    jump to java

--- src/Sample4.java
+++ src/Sample4.java
@@ -1,20 +1,36 @@
-class Updater {
-    void update(Counter counter) {   // 메소드
-        counter.count++;
+class Animal {
+    String name;
+
+    void setName(String name) { // setName : 메소드, name : 객체 변수
+        this.name = name;
     }
 }
 
-class Counter {
-    int count = 0;                   // 객체변수
+class Dog extends Animal {
+    void sleep() {  // sleep : 메소드
+        System.out.println(this.name + " zzz");
+    }
+}
+
+class HouseDog extends Dog {
+
+    HouseDog(String name) {
+        this.setName(name);
+    }
+    void sleep() {  // sleep : 메소드
+        System.out.println(this.name + " zzz in house");
+    }
+
+    void sleep(int hour) { // sleep: 메소드, hour : 객체 변수
+        System.out.println(this.name + " zzz in house for " + hour + " hours");
+    }
 }
 
 public class Sample4 {
     public static void main(String[] args) {
-        Counter myCounter = new Counter();
-        System.out.println("before update : " + myCounter.count);
-        Updater myUpdater = new Updater();
-        myUpdater.update(myCounter);
-        System.out.println("after update: " + myCounter.count);
+        HouseDog dog = new HouseDog("happy");
+        System.out.println(dog.name);   // null 출력
 
+        int a = 1;
     }
 }
