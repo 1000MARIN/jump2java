cec29c7fcbfdb9bc3dd98bff404f881d525ab093
1000marin <leexjin@gmail.com>
2022-06-21 09:47:42 +0900

    jump to java

--- src/Sample4.java
+++ src/Sample4.java
@@ -1,36 +1,19 @@
 class Animal {
     String name;
 
-    void setName(String name) { // setName : 메소드, name : 객체 변수
+    public void setName(String name) {
         this.name = name;
     }
 }
 
 class Dog extends Animal {
-    void sleep() {  // sleep : 메소드
-        System.out.println(this.name + " zzz");
-    }
+
 }
-
-class HouseDog extends Dog {
-
-    HouseDog(String name) {
-        this.setName(name);
-    }
-    void sleep() {  // sleep : 메소드
-        System.out.println(this.name + " zzz in house");
-    }
-
-    void sleep(int hour) { // sleep: 메소드, hour : 객체 변수
-        System.out.println(this.name + " zzz in house for " + hour + " hours");
-    }
-}
-
 public class Sample4 {
     public static void main(String[] args) {
-        HouseDog dog = new HouseDog("happy");
-        System.out.println(dog.name);   // null 출력
-
-        int a = 1;
+        Dog dog = new Dog();
+        dog.setName("puppy");
+        System.out.println(dog.name);   // puppy 출력
     }
+
 }
