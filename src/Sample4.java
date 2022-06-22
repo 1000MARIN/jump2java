858f64c49ec82cd9e06d5fa194237577e49f69cb
1000marin <leexjin@gmail.com>
2022-06-22 09:18:39 +0900

    jump to java

--- src/Sample4.java
+++ src/Sample4.java
@@ -7,13 +7,16 @@ class Animal {
 }
 
 class Dog extends Animal {
-
+    void sleep() {
+        System.out.println(this.name+" zzz");
+    }
 }
 public class Sample4 {
     public static void main(String[] args) {
         Dog dog = new Dog();
         dog.setName("puppy");
         System.out.println(dog.name);   // puppy 출력
+        dog.sleep();
     }
 
 }
