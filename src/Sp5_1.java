d3a2dc2fd1c50218c4767a52501b30008b07c8b4
1000marin <leexjin@gmail.com>
2022-06-27 11:15:23 +0900

    jump to java

--- src/Sp5_1.java
+++ src/Sp5_1.java
@@ -1,21 +1,30 @@
-class Calculator {
-    Integer value;
+interface Predator1 {
+    String bark();
+}
 
-    Calculator() {
-        this.value = 0;
+abstract class Animal1 {
+    public String hello() {
+        return "hello";
     }
-    void add(int val) {
-        this.value += val;
-    }
+}
 
-    public Integer getValue() {
-        return this.value;
+class Dog extends Animal1 {
+}
+
+class Lion1 extends Animal1 implements Predator1 {
+    public String bark() {
+        return "Bark bark!!";
     }
 }
 public class Sp5_1 {
     public static void main(String[] args) {
-        Calculator cal = new Calculator();
-        cal.add(8);     // 여기서 NullPointerException 이 발생한다.
-        System.out.println(cal.getValue());
+        Animal1 a = new Lion1();
+        Lion1 b = new Lion1();
+        Predator1 c = new Lion1();
+
+        System.out.println(a.hello());
+        System.out.println(b.bark());
+        System.out.println(b.hello());
+        System.out.println(c.bark());
     }
 }
